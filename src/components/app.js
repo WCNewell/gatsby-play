5975ae2de7fb8d85569fde834878b55b1e020b24
W. Clark Newell <clark@clarknewell.tech>
2020-03-23 17:51:11 -0600

    Experiementing with global styles
    Experiementing with global styles, may not play well with gatsby...

--- src/components/app.js
+++ src/components/app.js
@@ -8,9 +8,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
+import './layout.css'
 import Header from "./header"
-import "./layout.css"
 
 const App = ({ children }) => {
   const data = useStaticQuery(graphql`
