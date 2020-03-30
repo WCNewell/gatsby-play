7a8eb4f77040b6a658a47f506ce3ad46862037e4
W. Clark Newell <clark@clarknewell.tech>
2020-03-30 14:39:45 -0600

    CSS burger menu working
    CSS burger menu working but drawer still sliding from left

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { ThemeProvider } from 'styled-components';
+import { useOnClickOutside } from '../hooks';
 import { GlobalStyles } from './global';
 import { theme } from './theme';
+import { Burger, Menu } from './Navigation'
+
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const node = useRef();
+  const menuId = "main-menu";
+
+  useOnClickOutside(node, () => setOpen(false));
+
   return (
     <ThemeProvider theme={theme}>
       <>
         <GlobalStyles />
+        <div ref={node}>
+          <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
+          <Menu open={open} setOpen={setOpen} id={menuId} />
+        </div>
         <div>
           <h1>Hello. This is burger menu tutorial</h1>
           <img src="https://image.flaticon.com/icons/svg/2016/2016012.svg" alt="burger icon" />
-          <small>Icon made by Freepik from www.flaticon.com</small>
+          <small>Icon made by <a href="https://www.freepik.com/home">Freepik</a> from <a href="https://www.flaticon.com">www.flaticon.com</a></small>
         </div>
       </>
     </ThemeProvider>
   );
 }
-export default App;
+export default App
