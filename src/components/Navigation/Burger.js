97a17c4c20be6c20bf59652da349e627694c3c72
W. Clark Newell <clark@clarknewell.tech>
2020-03-30 19:14:19 -0600

    CSS-in-JS burger menu drawer right side working with react hooks in gatsby
    CSS-in-JS burger menu drawer right side working with react hooks in gatsby

--- src/components/Navigation/Burger.js
+++ src/components/Navigation/Burger.js
@@ -1,4 +1,26 @@
-import styled from 'styled-components';
+import React from 'react';
+import { bool, func } from 'prop-types';
+import styled from 'styled-components'
+
+const Burger = ({ open, setOpen, ...props }) => {
+  
+  const isExpanded = open ? true : false;
+  
+  return (
+    <StyledBurger aria-label="Toggle menu" aria-expanded={isExpanded} open={open} onClick={() => setOpen(!open)} {...props}>
+      <span />
+      <span />
+      <span />
+    </StyledBurger>
+  )
+}
+
+Burger.propTypes = {
+  open: bool.isRequired,
+  setOpen: func.isRequired,
+};
+
+export default Burger;
 
 export const StyledBurger = styled.button`
   position: absolute;
@@ -38,4 +60,4 @@ export const StyledBurger = styled.button`
       transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
     }
   }
-`;
+`
