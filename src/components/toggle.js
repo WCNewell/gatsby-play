41b2d55bd53af73fd47b638b5550fe6215a5ebc3
W. Clark Newell <clark@clarknewell.tech>
2020-04-02 13:31:31 -0600

    Created styled toggle component, but not working yet
    Created styled toggle component, but not working yet

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -8,10 +8,10 @@ import { ReactComponent as SunIcon } from '../images/iconfinder_sun_simple_36752
 const Toggle = ({ theme, toggleTheme }) => {
     const isLight = theme === 'light'
     return (
-        <button onClick={toggleTheme}>
+        <ToggleContainer onClick={toggleTheme}>
             <SunIcon />
             <MoonIcon />
-        </button>
+        </ToggleContainer>
     )
 }
 
@@ -22,3 +22,33 @@ Toggle.propTypes = {
 
 export default Toggle
 
+const ToggleContainer = styled.button`
+    background: ${({ theme }) => them.gradient}
+    border: 2px solid ${({ theme }) => theme.toggleBorder}
+    border-radius: 30px;
+    cursor: pointer;
+    display: flex;
+    font-size: 0.5rem;
+    justify-content: space-between;
+    margin: 0 auto;
+    overflow: hidden;
+    padding: 0.5rem;
+    position: relative;
+    width: 8rem;
+    height: 4rem;
+
+    img {
+        height: auto;
+        width: 2.5rem;
+        transition: all 0.3s linear;
+
+        // sun icon
+        &:first-child {
+            transform: ${({ lightTheme }) => lightTheme ? 'translateY(0)' : 'translateY(100px)'}
+        
+        // moon icon
+        &:second-child {
+            transform: ${({ lightTheme }) => lightTheme ? 'translateY(-100px)' : 'translateY(0)'} 
+        }
+    }
+`
