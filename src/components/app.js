563cc4948da5cd0da37662b3cba49969c2077c44
W. Clark Newell <clark@clarknewell.tech>
2020-04-01 18:46:02 -0600

    Moved burger menu to new header component
    Moved burger menu to new header component

--- src/components/app.js
+++ src/components/app.js
@@ -1,31 +1,16 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { lightTheme, darkTheme } from './theme'
-import { useOnClickOutside } from '../hooks';
 import { GlobalStyles } from './global';
-import { theme } from './theme';
-import { Burger, Menu } from './Navigation'
-import styled from 'styled-components'
-import SunIcon from '../images/iconfinder_sun_simple_367526.png'
+import Header from './header'
 
-function App() {
-  const [open, setOpen] = useState(false);
-  const node = useRef();
-  const menuId = "main-menu";
-
-  useOnClickOutside(node, () => setOpen(false));
-
+const App = () => {
+  
   return (
     <ThemeProvider theme={darkTheme}>
       <>
         <GlobalStyles />
-        <div ref={node}>
-            <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
-            <Menu open={open} setOpen={setOpen} id={menuId} />
-        </div>
-        <modeButton>
-          <img className='mode-button' src={SunIcon} alt="sun icon light mode button" />
-        </modeButton>
+        <Header />
         <div>
           <h1>Hello. This is a burger menu demo</h1>
         </div>
@@ -34,18 +19,11 @@ function App() {
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-      </>
+      </>  
     </ThemeProvider>
   )
 }
 
 export default App
 
-const modeButton = styled.button`
-  display: flex;
-  
-  img {
-    height: 36px;
-    width: 36px;
-  }
-`
+
