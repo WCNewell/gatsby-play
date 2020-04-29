7a7b69740b7e261066b758df865deed6a8d001ef
W. Clark Newell <clark@clarknewell.tech>
2020-04-28 18:01:18 -0600

    svg icons imported and rendering
    added gatsby svg svg icons imported and rendering, also started working with star background css

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,16 @@
 import React, { useState }from 'react'
 import { ThemeProvider } from 'styled-components'
-import { GlobalStyles } from './global'
-import { lightTheme, darkTheme, starTheme } from './theme';
+import lightTheme from ;
 import Nav from './nav'
 import styled from 'styled-components'
 
-import SunIcon from '../images/sunpng.png'
-import MoonIcon from '../images/moonpng.png'
-import StarIcon from '../images/starpng.png'
+// import SunIcon from '../images/sunpng.png'
+// import MoonIcon from '../images/moonpng.png'
+// import StarIcon from '../images/starpng.png'
+
+import SunIcon from '../assets/sun-svg.svg'
+import MoonIcon from '../assets/moon-svg.svg'
+import StarsIcon from '../assets/stars-svg.svg'
 
 const App = () => {
     const [theme, setTheme] = useState(lightTheme)
@@ -33,18 +36,11 @@ const App = () => {
     return (
         <ThemeProvider theme={theme}>
             <>
-                <GlobalStyles />
                <>
-                    <ModeButton onClick={toggleLightTheme}>
-                        <img src={SunIcon} alt="Sun icon for light mode" />
-                    </ModeButton>
-                    <ModeButton onClick={toggleDarkTheme}>
-                        <img src={MoonIcon} alt="Moon icon for dark mode" />
-                    </ModeButton>
-                     <ModeButton onClick={toggleStarTheme}>
-                        <img src={StarIcon} alt="Star icon for star mode" />
-                    </ModeButton>
                     
+                    <SunIcon onClick={toggleLightTheme} alt='sun icon for light theme' />
+                    <MoonIcon onClick={toggleDarkTheme} alt='moon icon for dark theme' />
+                    <StarsIcon onClick={toggleStarTheme} alt='stars icon for star theme'/>
                 </>
                 <Nav />
                 <div>
@@ -69,15 +65,13 @@ const ModeButton = styled.div`
     flex-flow: row norwap;
     justify-content: flex-start;
     align-items: center;
-    width: auto;
     padding-bottom: 10px;
+    fill: gold;
+    width: 50px;
+    height: 50px;
 
-    img {
-        width: 50px;
-        height: 50px;
-
-        &:first-of-type {
+    &:first-of-type {
             margin-right: 10px;
-        }
     }
+    
 `
