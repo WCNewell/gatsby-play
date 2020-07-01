bb0cf9faa934f05ad1c045199c9857e53317b09a
William Clark Newell <clark@clarknewell.tech>
2020-07-01 07:58:34 -0600

    Triple mode rendering with update to root-wrapper
    Triple mode rendering with update to root-wrapper

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import App from './src/components/app'
-import { GlobalStyles } from './src/styles/global'
 
 export const wrapRootElement = ({ element }) => (
     <>
-        <GlobalStyles />
         <App>{element}</App>
     </>
 )
