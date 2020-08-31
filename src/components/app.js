c4774433e7705fdde34ffca93f0981f8fd90192d
William Clark Newell <clark@clarknewell.tech>
2020-08-30 20:50:58 -0600

    Keep on keepin on
    Keep on keepin on

--- src/components/app.js
+++ src/components/app.js
@@ -1,85 +1,64 @@
-import React, { useState, useContext } from 'react'
+import React from 'react'
 import useDarkMode from 'use-dark-mode'
 import Header from './header'
 import Footer from './footer'
 
 import SunIcon from '../assets/sun.inline.svg'
 import MoonIcon from '../assets/moon.inline.svg'
-import StarsIcon from '../assets/stars.inline.svg'
 
+import StarLayout from './star-layout'
+// import lightTheme from './light-theme'
+// import darkTheme from './dark-theme'
 import styled from 'styled-components'
-import '../styles/styles.scss'
+import '../styles/global-styles.scss'
 
-const themes = {
-    light: {
-        text: '#0d0c1d'
-    },
-    dark: {
-        text: '#effffa'
-    }
-}
-
-const ThemeContext = React.createContext(themes.light)
+export const ThemeContext = React.createContext('light')
 
 const App = ({ children }) => {
-    const [theme, setTheme] = useState(themes.light)
+    const [theme, setTheme] = React.useState('light')
     const darkMode = useDarkMode(false)
-    const mode = useContext(ThemeContext)
-    
-    
-    // const toggleLightTheme = () => {
-    //     if (theme === 'dark' || 'star') {
-    //         setTheme('light')
-    //     }
-    // }
-    
-    // const toggleDarkTheme = () => {
-    //     if (theme === 'light' || 'star') {
-    //         setTheme('dark')
-    //     }
-    // }
-    
-    // const toggleStarTheme = () => {
-    //     if (theme === 'light' || 'dark') {
-    //         setTheme('star')
-    //     }
-    // }
 
+    
+    const toggleTheme = () => {
+        theme === darkMode ? setTheme('dark') : setTheme('light')
+    }
+    
     return (
-        <ThemeContext.Provider value={theme}>
-            <div style={{ color: mode.text }} >
+        <ThemeContext.Provider value={'dark'}>
+        <>
+            <Layout />
                 <ModeIcons>
                     <SunIcon    className='mode-icon'
                                 onClick={() => {
-                                    // toggleLightTheme()
+                                    toggleTheme()
                                     darkMode.disable()
                                 }}
                                 alt='sun icon for light mode'
                     />
                     <MoonIcon   className='mode-icon' 
                                 onClick={() => {
-                                    // toggleDarkTheme()
+                                    toggleTheme()
                                     darkMode.enable()
                                 }}
                                 alt='moon icon for dark mode'
-                    />
-                    <StarsIcon  className='mode-icon'
-                                onClick={() => {
-                                    // toggleStarTheme()
-                                    darkMode.enable()
-                                }}
-                                alt='stars icon for star mode'
                     />
                 </ModeIcons>
                 <Header />
                     {children}
                 <Footer />
-            </div>
+        </>
         </ThemeContext.Provider>
     )
 }
 
 export default App
+
+const Layout = () => {
+    const theme = React.useContext(ThemeContext)
+    return (
+        theme === 'dark' ? <StarLayout /> : null
+    )
+}
 
 const ModeIcons = styled.div`
     display: flex;
